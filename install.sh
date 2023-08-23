73fe2e9b00b9439d5272993ac6fca27147aaee3b
DarkNight <57034210+BeastGamer81@users.noreply.github.com>
2023-08-23 15:53:16 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -282,13 +282,19 @@ case $n in
 
     curl -o server.jar https://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar
 
+    validateJavaVersion
+
+    # Remove 200 mb to prevent server freeze
+    number=200
+    memory=$((SERVER_MEMORY - number))
+   
     display
     
     sleep 10
 
     echo -e ""
 
-    launchJavaServer
+    java -Xms128M -Xmx${memory}M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar nogui																																																																																																																													
   ;;
   4)
   echo "$(tput setaf 3)Starting Download please wait"
