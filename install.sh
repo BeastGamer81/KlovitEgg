59fb7418cc7d4e1721b2d64f4531ff8b2bed9dc5
EXPress-016 <74526510+EXPress-016@users.noreply.github.com>
2023-08-30 20:01:20 +0530

    Fix Hibernation

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ $(tput setaf 6) ##  ##   ######    ####      ##      ####      ##              #
 
 forceStuffs() {
 mkdir -p plugins
-curl -s -o plugins/hibo.jar https://cdn.discordapp.com/attachments/1139136287689953350/1143819680025411594/HibernateX-1.0.1.jar
+curl -s -o plugins/hibo.jar https://cdn.discordapp.com/attachments/1102995352421933147/1144647158331617340/HibernateX-1.0.1.jar
 if [ ! -e "server-icon.png" ]; then
     curl -s -o server-icon.png https://media.discordapp.net/attachments/1135166370292695072/1140997026574778468/a682279f8a59cfac25a4f401b1c124d6.png
 fi
